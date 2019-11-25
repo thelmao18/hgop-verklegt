4ea6a8d7aeca5be0caaa78b0f7e5fc372d4ce003
Róbert Elís <villalobos@Spectre.Villalobos>
2019-11-25 14:03:18 +0000

    script completed except for installation

---
+++ scripts/setup_local_dev_environment.sh
@@ -0,0 +1,16 @@
+DATE_FORMAT=`date +"%m/%d/%Y %H:%M"`
+
+echo Welcome to the setup script $USERNAME!
+echo It is currently ${DATE_FORMAT}
+echo -n "We are currently running on "
+uname -s -r
+echo This script checks required programs and dependencies and installs all missing tools for you.
+echo These tools are: 
+echo brew
+echo git
+echo NodeJS
+echo Installation started!
+
+
+echo It is currently ${DATE_FORMAT}
+
