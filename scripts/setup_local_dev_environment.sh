24621fbf7c2222e5ea874b4c8f69de3b6a3efc71
Lobos98 <robertelis98@gmail.com>
2019-11-25 15:39:04 +0000

    added comments

--- scripts/setup_local_dev_environment.sh
+++ scripts/setup_local_dev_environment.sh
@@ -3,25 +3,31 @@
 
 execute_script()
 {
-
+    #we define the whole script as a function to be able to pipe its output into tee 
     DATE_FORMAT=`date +"%m/%d/%Y %H:%M"`
     OP_SYS=`uname -s`
+    #initialize environment variables
+
 
     echo Welcome to the setup script $USERNAME!
     echo It is currently ${DATE_FORMAT}
-    echo -n "We are currently running on "
-    uname -s -r
+    echo "We are currently running on $OP_SYS"
     echo This script checks required programs and dependencies and installs all missing tools for you.
     echo These tools are: 
     echo brew
     echo git
     echo NodeJS
     echo Installation started!
+    #Print various messages
+
+
     if [ $OP_SYS = Linux ]
     then
+	    #If we are running Linux, install dependecies with get-apt
 	    sudo apt-get install git
 	    sudo apt-get install nodejs
 	    sudo apt-get install npm
+	    #check nodejs version since MacOS and Linux use different names
 	    echo -n "nodejs "
 	    nodejs --version
 
@@ -29,14 +35,17 @@ execute_script()
 
     if [ $OP_SYS = Darwin ]
     then
+	    #IF we are running MacOS, install brew and use it to install dependencies
 	    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	    brew install git
 	    brew install node
+	    #See line 30
 	    brew --version
 	    echo -n "nodejs "
 	    node --version
     fi
 
+    #Print version
     git --version
     echo -n "npm "
     npm --version
@@ -44,8 +53,10 @@ execute_script()
 
     echo Installation complete!
     echo It is currently ${DATE_FORMAT}
+    #Confirm installation and print date
 }
 
 execute_script | tee output.log
+#Execute function and pipe output to file and stdout
 
 
