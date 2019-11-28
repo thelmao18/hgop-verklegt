d9e25da60205b20140973f3abbecf1b40a1df264
Lobos98 <robertelis98@gmail.com>
2019-11-28 13:06:57 +0000

    Finished deploy and added *.swp to gitignore

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-
+terraform destroy -auto-approve
+terraform apply -auto-approve
+ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./initialize_game_api_instance.sh"
+exit
