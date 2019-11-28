38da71efe30417c5f3858c59ec72f303e66b0561
Lobos98 <robertelis98@gmail.com>
2019-11-28 12:47:51 +0000

    finished and tested infrastructre.tf, most comments completed and deploy script created but not written

--- infrastructure.tf
+++ infrastructure.tf
@@ -1,12 +1,100 @@
+#This block specifies the cloud provider and location to host on
+#and locates the credentials needed to use the AWSclient to access
+#and control my AWS account
 provider "aws" {
   shared_credentials_file = "~/.aws/credentials"
   region                  = "us-east-1"
 }
 
+#This block defines how Terraform will configure the AWS server's security
+#rules, allowing it to ssh into the server using predefined ports.
+resource "aws_security_group" "game_security_group" {
+  name = "GameSecurityGroup"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#This block tells Terraform to create an AWS server called game_server 
+#and defines its basic settings, such as the instance type. Also 
+#accepts the name of the private key used to ssh into the server.
 resource "aws_instance" "game_server" {
-  ami           = "ami-0ac019f4fcb7cb7e6"
-  instance_type = "t2.micro"
+  ami                    = "ami-0ac019f4fcb7cb7e6"
+  instance_type          = "t2.micro"
+  key_name               = "GameKeyPair"
+  vpc_security_group_ids = [aws_security_group.game_security_group.id]
   tags = {
     Name = "GameServer"
   }
+
+  # This block finds a script file on my computer using a relative path and 
+  # specifies a destination on the AWS server for it to be copied to. Sends it over.
+  provisioner "file" {
+    source      = "scripts/initialize_game_api_instance.sh"
+    destination = "/home/ubuntu/initialize_game_api_instance.sh"
+
+    connection {
+      host        = coalesce(self.public_ip, self.private_ip)
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.aws/GameKeyPair.pem")
+    }
+  }
+
+  # Similar to above, finds and sends the docker composition configuration file
+  # over to the AWS cloud server so our system can be correctly assembled.
+  provisioner "file" {
+    source      = "docker-compose.yml"
+    destination = "/home/ubuntu/docker-compose.yml"
+
+    connection {
+      host        = coalesce(self.public_ip, self.private_ip)
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.aws/GameKeyPair.pem")
+    }
+  }
+
+  # This is used to run commands on the instance we just created.
+  # Terraform does this by SSHing into the instance and then executing the commands.
+  # Since it can take time for the SSH agent on machine to start up we let Terraform
+  # handle the retry logic, it will try to connect to the agent until it is available
+  # that way we know the instance is available through SSH after Terraform finishes.
+  # 
+  # TODO comment 1-2 sentences
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ubuntu/initialize_game_api_instance.sh",
+    ]
+
+    connection {
+      host        = coalesce(self.public_ip, self.private_ip)
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.aws/GameKeyPair.pem")
+    }
+  }
+}
+
+# TODO Comment 1-2 sentences.
+output "public_ip" {
+  value = aws_instance.game_server.public_ip
 }
