cf189f9e883f2373cc9d092d3c7152da56d93505
Lobos98 <robertelis98@gmail.com>
2019-12-03 16:12:17 +0000

    2 tests and added missing comment in lucky21.js

--- game_api/lucky21.unit-test.js
+++ game_api/lucky21.unit-test.js
@@ -60,7 +60,7 @@ test('isGameOver', () => {
 
 test('', () => {
   //TODO
-
+  
   // Inject our dependencies
   let game = lucky21Constructor(deck, dealer);
 });
@@ -149,23 +149,48 @@ test('', () => {
   let game = lucky21Constructor(deck, dealer);
 });
 
-test('', () => {
-  //TODO
-
+test('getTotal should be lower than 22 if you have an ace and roll over 21', () => {
+  // Arrange
+  let deck = deckConstructor();
+  deck = [
+      '05C', '09D', '01S', '10H', 
+  ];
+  let dealer = dealerConstructor();
+  // Override the shuffle to do nothing.
+  dealer.shuffle = (deck) => {};
+  
   // Inject our dependencies
   let game = lucky21Constructor(deck, dealer);
+  
+  // act
+  game.guess21OrUnder(game);
+
+  
+  // Assert
+  expect(game.getTotal(game)).to.be.below(22)
+});
+
+test('getCards should return the last 2 cards in the deck after the shuffle', () => {
+  // Arrange
+  let deck = deckConstructor();
+  let dealer = dealerConstructor();
+  dealer.shuffle(deck);
+
+  //save the last 2 cards in an array
+  card0 = deck[49];
+  card1 = deck[48];
+  my_cards = [card0, card1];
+  // Override the shuffle to do nothing.
+  dealer.shuffle = (deck) => {};
+
+  
+  // Inject our dependencies
+  let game = lucky21Constructor(deck, dealer);
+
+  // Assert
+  expect(game.getCards).toEqual(my_cards);
 });
 
 test('', () => {
   //TODO
-
-  // Inject our dependencies
-  let game = lucky21Constructor(deck, dealer);
-});
-
-test('', () => {
-  //TODO
-
-  // Inject our dependencies
-  let game = lucky21Constructor(deck, dealer);
 });
