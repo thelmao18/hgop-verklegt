ecef5e01dfe5fecf829ddc51abeee25658c44f1a
Lobos98 <robertelis98@gmail.com>
2019-12-03 17:27:02 +0000

    getCard and getCardValues tests

--- game_api/lucky21.unit-test.js
+++ game_api/lucky21.unit-test.js
@@ -246,35 +246,68 @@ test('getCardsValue', () => {
   let game = lucky21Constructor(deck, dealer);
 });
 
-test('getCardValue', () => {
-  //TODO
+test('getCardValue should return undefined if player chooses guess21OrUnder', () => {
+  //Arrange
+  let deck = deckConstructor();
+  let dealer = dealerConstructor();
 
   // Inject our dependencies
   let game = lucky21Constructor(deck, dealer);
+
+  //Act
+  game.guess21OrUnder(game);
+
+  //Assert
+  expect(game.getCardValue(game)).toEqual(undefined);
 });
 
-test('getCard', () => {
-  //TODO
+test('getCardValue should return int after choosing guessOver21', () => {
+  //Arrange
+  let deck = deckConstructor();
+  let dealer = dealerConstructor();
 
   // Inject our dependencies
   let game = lucky21Constructor(deck, dealer);
+
+  //Act
+  game.guessOver21(game);
+
+  //Assert
+  expect(game.getCardValue(game)).to.be.a(number);
 });
 
-test('', () => {
-  //TODO
+test('getCard should return undefined after choosing guess21OrUnder', () => {
+  //Arrange
+  let deck = deckConstructor();
+  let dealer = dealerConstructor();
 
   // Inject our dependencies
   let game = lucky21Constructor(deck, dealer);
+
+  //Act
+  game.guess21OrUnder(game);
+
+  //Assert
+  expect(game.getCard(game)).toEqual(undefined);
+
 });
 
-test('', () => {
-  //TODO
+test('getCard should return string after choosing guessOver21', () => {
+  //Arrange
+  let deck = deckConstructor();
+  let dealer = dealerConstructor();
 
   // Inject our dependencies
   let game = lucky21Constructor(deck, dealer);
+
+  //Act
+  game.guessOver21(game);
+
+  //Assert
+  expect(game.getCard(game)).to.be.a(String);
 });
 
-test('getTotal should be lower than 22 if you have an ace with the value 10 and have not yet lost', () => {
+test('getTotal should return a different value after choosing ', () => {
   // Arrange
   let deck = deckConstructor();
   deck = [
@@ -297,8 +330,8 @@ test('getCards should return the last 2 cards in the deck after the shuffle', ()
   dealer.shuffle(deck);
 
   //save the last 2 cards in an array
-  let card0 = deck[49];
-  let card1 = deck[48];
+  let card0 = deck[51];
+  let card1 = deck[50];
   let my_cards = [card0, card1];
   // Override the shuffle to do nothing.
   dealer.shuffle = (deck) => {};
@@ -310,23 +343,6 @@ test('getCards should return the last 2 cards in the deck after the shuffle', ()
   expect(game.getCards(game)).toEqual(my_cards);
 });
 
-test('guessOver21 should draw the next card', () => {
-  // Arrange
-  let deck = deckConstructor();
-  deck = [
-      '05C', '01D', '09S', '10H', 
-  ];
-  let dealer = dealerConstructor();
-  // Override the shuffle to do nothing.
-  dealer.shuffle = (deck) => {};
-  
-  // Inject our dependencies
-  let game = lucky21Constructor(deck, dealer);
-  
-  // Act
-  game.guessOver21(game);
-  
-  // Assert
-  expect(game.state.cards.length).toEqual(3);
-  expect(game.state.cards[2]).toEqual('01D');
+test('', () => {
+  //TODO
 });
