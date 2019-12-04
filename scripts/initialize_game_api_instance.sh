ca90e843c8d6504aa952523dd73e27a1d931a9d5
Lobos98 <robertelis98@gmail.com>
2019-12-04 18:36:20 +0000

    double docker install

--- scripts/initialize_game_api_instance.sh
+++ scripts/initialize_game_api_instance.sh
@@ -17,6 +17,20 @@ sudo add-apt-repository \
     stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+echo REPRISE
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+
+
+
 sudo groupadd docker
 sudo usermod -aG docker ubuntu
 
