faa3dc0cc0a5aac67c72b93e9599e7e50829a141
Lobos98 <robertelis98@gmail.com>
2019-12-04 18:21:39 +0000

    made docker_compose_up.sh executable on the server

--- scripts/jenkins_deploy.sh
+++ scripts/jenkins_deploy.sh
@@ -22,8 +22,11 @@ terraform apply -auto-approve
 
 echo "Game API running at " + $(terraform output public_ip)
 
-ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./initialize_game_api_instance.sh" ｜｜ exit 1
-ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./docker_compose_up.sh $GIT_COMMIT" ｜｜ exit 1
+ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./initialize_game_api_instance.sh"
+ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "chmod +x docker_compose_up.sh"
+ssh -o StrictHostKeyChecking=no -i "~/.aws/GameKeyPair.pem" ubuntu@$(terraform output public_ip) "./docker_compose_up.sh $GIT_COMMIT"
+
+curl ubuntu@$(terraform output public_ip):3000/status ｜｜ exit 1
 
 
 exit 0
