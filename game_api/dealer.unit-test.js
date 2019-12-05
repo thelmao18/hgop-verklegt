3e7f329eeb472207f2bf639e6a67181f01856731
Lobos98 <robertelis98@gmail.com>
2019-12-05 19:14:53 +0000

    added tests for shuffle and draw functions

--- game_api/dealer.unit-test.js
+++ game_api/dealer.unit-test.js
@@ -1,5 +1,6 @@
 const context = require('./context.js').newContext();
 const dealerConstructor = require('./dealer.js');
+const deckConstructor = require("./deck.js");
 
 function newRandom(randomReturnValues) {
   let i = 0;
@@ -29,6 +30,7 @@ test('dealer should shuffle cards', () => {
 });
 
 test('shuffle should return a deck of equal length to the original deck', () => {
+  //Arrange
   const dealer = dealerConstructor(context);
   const deck = ['a', 'b', 'c', 'd', 'e', 'f'];
 
@@ -39,14 +41,46 @@ test('shuffle should return a deck of equal length to the original deck', () => 
   expect(deck.length).toEqual(6);
 });
 
-test('shuffle', () => {
+test('shuffle should include all cards originally in the deck', () => {
+  //Arrange
+  const dealer = dealerConstructor(context);
+  let deck = ['a', 'b', 'c', 'd', 'e', 'f'];
+  const deck_copy = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+  // Act
+  dealer.shuffle(deck);
+
+  //Assert
+  expect(deck).toEqual(expect.arrayContaining(deck_copy));
 
 });
 
-test('draw', () => {
+test('draw should remove a card from deck', () => {
+  //Arrange
+  const dealer = dealerConstructor(context);
+  let deck = deckConstructor(context);
+
+  //Act
+
+  let my_card = dealer.draw(deck);
+
+  //Assert
+  expect(deck).toEqual(expect.not.arrayContaining([my_card]));
 
 });
 
-test('draw', () => {
+test('draw should always reduce deck length by one', () => {
+  //Arrange
+  const dealer = dealerConstructor(context);
+  let deck = deckConstructor(context);
+
+  //Act
+  let old_length = deck.length;
+  let my_card = dealer.draw(deck);
+  let new_length = deck.length;
+
+  //Assert
+  expect(new_length).toEqual(old_length-1);
+
 
 });
