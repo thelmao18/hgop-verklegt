b46cea32c2352919734442a2e47810f84a5f9256
Thelma Ólafsdóttir <thelmao18@ru.is>
2019-12-05 19:40:35 +0000

    Tests for deck and random

--- game_api/random.unit-test.js
+++ game_api/random.unit-test.js
@@ -1,11 +1,36 @@
-test('', () => {
-  // TODO
+const context = require('./context.js').newContext();
+const randomConstructor = require('./random.js');
+
+test('random should return a number', () => {
+    // Arrange
+    const game = randomConstructor(context);
+
+    // Act
+    let randomNumber = game.randomInt(game);
+
+    // Assert
+    expect(typeof randomNumber).toEqual('number');
 });
 
-test('', () => {
-  // TODO
+test('random should return a random number between 5 & 10', () => {
+  // Arrange
+  const game = randomConstructor(context);
+
+  // Act
+  let randomNumber = game.randomInt(5, 10);
+
+  // Assert
+  expect(randomNumber).toBeGreaterThanOrEqual(5)
+  expect(randomNumber).toBeLessThanOrEqual(10);
 });
 
-test('', () => {
-  // TODO
+test('random should return a number less than 5', () => {
+  // Arrange
+  const game = randomConstructor(context);
+
+  // Act
+  let randomNumber = game.randomInt(0, 5);
+
+  // Assert
+  expect(randomNumber).toBeLessThan(5);
 });
