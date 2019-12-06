6f0823a0ecd4117391d266ec04c2efac6242e28d
Lobos98 <robertelis98@gmail.com>
2019-12-06 14:41:26 +0000

    now using updated initialize_game_api_instance script from issue on GitHub

--- scripts/initialize_game_api_instance.sh
+++ scripts/initialize_game_api_instance.sh
@@ -4,7 +4,8 @@ echo 'This script installs everything needed to run our API on the instance'
 echo 'and then starts the API.'
 
 echo 'Installing Docker'
-sudo apt-get install -y \
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -15,25 +16,16 @@ sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-
-echo REPRISE
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-
-
-
-
-sudo groupadd docker
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker ubuntu
 
 echo 'Install Docker Compose'
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose || exit 1
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Check if docker-compose.yml file is present or not.
+if [ ! -f ./docker-compose.yml ]; then
+    echo "docker-compose.yml file is not present"
+    exit 1
+fi
