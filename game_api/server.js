73ba684516c2c10e80614904012bd9bec9875361
Lobos98 <robertelis98@gmail.com>
2019-12-09 12:44:25 +0000

    answered TODO in server.js

--- game_api/server.js
+++ game_api/server.js
@@ -27,6 +27,13 @@ module.exports = function(context) {
           //database.call1(...);
           //database.call2(...);
           //database.call3(...);
+          /*
+          We put each consecutive call inside the onSuccess callback of the previous
+          database call to allow the code to continue without having to wait for each
+          call between services to finish. This would not be a good idea if we were writing
+          to a database where the order is important, but in this case we are reading from
+          a database that should not change while we make these calls.
+          */
 
           res.statusCode = 200;
           res.send({
