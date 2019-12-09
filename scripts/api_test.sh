e3b3ee7174f0e4df989539ed660dc54f877c6563
Lobos98 <robertelis98@gmail.com>
2019-12-09 16:21:07 +0000

    removed money

--- scripts/api_test.sh
+++ scripts/api_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /var/lib/jenkins/terraform/hgop/apitest
-API_URL=http://\$(terraform output public_ip):3000
+API_URL=http://$(terraform output public_ip):3000
 cd -
 npm run test:api
--- scripts/cap_test.sh
+++ scripts/cap_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /var/lib/jenkins/terraform/hgop/capacitytest
-API_URL=http://\$(terraform output public_ip):3000
+API_URL=http://$(terraform output public_ip):3000
 cd -
 npm run test:capacity
