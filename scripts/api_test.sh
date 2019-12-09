d407ef54141563dd5fbfe8d4e0dfcb54c6070c01
Lobos98 <robertelis98@gmail.com>
2019-12-09 16:27:28 +0000

    added debug lines

--- scripts/api_test.sh
+++ scripts/api_test.sh
@@ -2,5 +2,7 @@
 
 cd /var/lib/jenkins/terraform/hgop/apitest
 API_URL=http://$(terraform output public_ip):3000
+echo $API_URL
 cd -
+echo $API_URL
 npm run test:api
--- scripts/cap_test.sh
+++ scripts/cap_test.sh
@@ -2,5 +2,7 @@
 
 cd /var/lib/jenkins/terraform/hgop/capacitytest
 API_URL=http://$(terraform output public_ip):3000
+echo $API_URL
 cd -
+echo $API_URL
 npm run test:capacity
