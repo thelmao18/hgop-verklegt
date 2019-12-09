34a938ef58096f5206ed8c0ee9ee1bddbb6cb5aa
Lobos98 <robertelis98@gmail.com>
2019-12-09 15:32:50 +0000

    lints removed

--- game_api/server.api-test.js
+++ game_api/server.api-test.js
@@ -4,7 +4,7 @@ const timeout = 30000;
 
 // TODO what does the done parameter do?
 /*
-The done parameter is a non-official standard name for a function, a.k.a callback, 
+The done parameter is a non-official standard name for a function, a.k.a callback,
 that informs the calling function that a task is completed.
 */
 test('play a game', function(done) {
