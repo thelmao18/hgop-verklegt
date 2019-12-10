9f15e53760a5c822057250dd1011a562802b3c04
Lobos98 <robertelis98@gmail.com>
2019-12-10 13:10:35 +0000

    updated GameResultTable and removed duplicate. package.json saved after updating dependencies

--- game_api/migrations/20191210120722-GameResultTable.js
+++ game_api/migrations/20191210120722-GameResultTable.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
 var dbm;
 var type;
 var seed;
 
 /**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
 exports.setup = function(options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
@@ -15,13 +15,18 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  return null;
+  return db.createTable("GameResult", {
+    ID: { type: "int", primaryKey: true, autoIncrement: true },
+    Won: { type: "boolean", notNull: true },
+    Score: { type: "int", notNull: true },
+    Total: { type: "int", notNull: true }
+  });
 };
 
 exports.down = function(db) {
-  return null;
+  return db.dropTable("GameResult");
 };
 
 exports._meta = {
-  "version": 1
+  version: 1
 };
