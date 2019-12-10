5bfed1884fc522d7e09307d3ccf6d42fa01b9bd1
Lobos98 <robertelis98@gmail.com>
2019-12-10 13:43:46 +0000

    updated config and database default hostname/username/passwords to match each other

--- game_api/config.js
+++ game_api/config.js
@@ -1,10 +1,10 @@
 module.exports = function(context) {
   return {
     // Postgres
-    pgHost: process.env.POSTGRES_HOST || 'default_host',
-    pgUser: process.env.POSTGRES_USER || 'default_user',
-    pgPassword: process.env.POSTGRES_PASSWORD || 'default_password',
-    pgDatabase: process.env.POSTGRES_DB || 'default_database',
+    pgHost: process.env.POSTGRES_HOST || 'my_postgres_container',
+    pgUser: process.env.POSTGRES_USER || 'my_postgres_user',
+    pgPassword: process.env.POSTGRES_PASSWORD || 'my_postgres_password',
+    pgDatabase: process.env.POSTGRES_DB || 'my_postgres_database',
 
     // Port
     port: process.env.PORT || 3000,
