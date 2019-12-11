662d2be0ed9c62e3ececefc292af3bb8b10d0149
Lobos98 <robertelis98@gmail.com>
2019-12-11 15:24:22 +0000

    changing sql queries, fixed Access Control and docker now pushes both images

--- game_client/src/utils.js
+++ game_client/src/utils.js
@@ -1,27 +1,27 @@
 import axios from "axios";
 // TODO: Add correct api Url
-const apiUrl = "";
+const apiUrl = "http://" + gRRgRR + ":3000";
 
 export const startGame = () => {
   // TODO: Call start game
-  return axios.post(`${apiUrl}`).then(res => {
+  return axios.post(`${apiUrl}` + "/start").then(res => {
     return getState();
   });
 };
 
 export const getState = () => {
   // TODO: Get the state of the game
-  return axios.get(`${apiUrl}`);
+  return axios.get(`${apiUrl}` + "/state");
 };
 
 export const guessOver21 = () => {
   // TODO: Guess over 21
-  return axios.post(`${apiUrl}`);
+  return axios.post(`${apiUrl}` + "/guessOver21");
 };
 
 export const guess21OrUnder = () => {
   // TODO: Guess 21 or under
-  return axios.post(`${apiUrl}`);
+  return axios.post(`${apiUrl}` + "/guess21OrUnder");
 };
 
 // Helper function to convert cards format
