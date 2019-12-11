133cbaab0733c5444e1d31c98cf467814939d672
Lobos98 <robertelis98@gmail.com>
2019-12-11 13:52:17 +0000

    game client build removed from jenkins

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -2,4 +2,4 @@
 
 GIT_COMMIT=$1
 docker build -t villalobos98/hgop:$GIT_COMMIT game_api/ || exit 1
-docker build -t villalobos98/game_client:$GIT_COMMIT game_client/ || exit 1
+#docker build -t villalobos98/game_client:$GIT_COMMIT game_client/ || exit 1
